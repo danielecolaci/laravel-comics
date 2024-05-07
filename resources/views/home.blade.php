01fc3fc67d4b4cac2dc61dd92622fe5ce7c5174a
danielecolaci <danielecolaci@outlook.com>
2024-05-07 15:43:24 +0200

    Add layout partials files and set homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, recusandae.</p>
+</div>
+
+@endsection
