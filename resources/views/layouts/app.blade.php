64480fb101e98fc4ad676ba51bc3e57ef51a249c
danielecolaci <danielecolaci@outlook.com>
2024-05-08 16:25:44 +0200

    Fix Header Style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel Comics')</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.ico') }}">
+    {{-- Font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     @vite('resources/js/app.js')
 </head>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,23 +1,29 @@
-<header class="navbar navbar-expand-lg navbar-light bg-light">
+<header class="navbar navbar-expand-lg">
     <div class="container">
         <div class="navbar-brand">
-            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo" width="50" height="50">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo" width="50" height="50">
         </div>
 
         <div class="collapse navbar-collapse justify-content-center">
-            <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link {{Route::currentRouteName() === 'comics' ? 'nav-active' :''}}" href="{{route('comics')}}" aria-current="page">Comics</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link {{Route::currentRouteName() === 'news' ? 'nav-active' :''}}" href="{{route('news')}}" aria-current="page">News</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link {{Route::currentRouteName() === 'shop' ? 'nav-active' :''}}" href="{{route('shop')}}" aria-current="page">Shop</a>
-            </li>
+            <ul class="navbar-nav gap-5">
+                <li class="nav-item">
+                    <a class="{{ Route::currentRouteName() === 'comics' ? 'nav-active' : '' }}"
+                        href="{{ route('comics') }}" aria-current="page">Comics</a>
+                </li>
+                <li class="nav-item">
+                    <a class="{{ Route::currentRouteName() === 'news' ? 'nav-active' : '' }}" href="{{ route('news') }}"
+                        aria-current="page">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="{{ Route::currentRouteName() === 'shop' ? 'nav-active' : '' }}" href="{{ route('shop') }}"
+                        aria-current="page">Shop</a>
+                </li>
             </ul>
         </div>
-
-        <input class="form-control w-25" type="search" placeholder="Search">
+        <div class="search">
+            <input type="search" placeholder="Search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
     </div>
 </header>
+<div class="jumbotron"></div>
